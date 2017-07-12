570dc1547f47beea5d99760337480633e7319a04
Chris Miranda <crmirand@ucsd.edu>
2017-07-11 22:46:37 -0700

    Added sandra

--- bot.js
+++ bot.js
@@ -46,6 +46,25 @@ function postCriMessage() {
     }
   ]
 }
+console.log('sending ' + botResponse + ' to ' + botID);
+
+  botReq = HTTPS.request(options, function(res) {
+      if(res.statusCode == 202) {
+        //neat
+      } else {
+        console.log('rejecting bad status code ' + res.statusCode);
+      }
+  });
+
+  botReq.on('error', function(err) {
+    console.log('error posting message '  + JSON.stringify(err));
+  });
+  botReq.on('timeout', function(err) {
+    console.log('timeout posting message '  + JSON.stringify(err));
+  });
+  botReq.end(JSON.stringify(body));
+}
+
 function postSandraMessage() {
   var botResponse, options, body, botReq;
 
